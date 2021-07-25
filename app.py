33aa96acb4f2782a104d5f6b8c5e8297d69f92c2
shloakr <shloakrathod1@gmail.com>
2021-07-26 00:48:24 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, url_for, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+    
+
+# @app.route('/predict', methods=['POST'])
+# def predict(): 
+        
+#     return render_template('result.html', results = arr)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
